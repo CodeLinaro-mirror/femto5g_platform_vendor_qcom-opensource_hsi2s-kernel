fff468394805caa4f515520781990472be103654
Vijayakumar Badiger <vbadig@codeaurora.org>
2019-11-21 11:55:01 -0800

    local_module_tags debug and eng is obsolete
    Change-Id: I3aafc355bfc929279d804f18886ec85deeaa5753

--- test/loopback/Android.mk
+++ test/loopback/Android.mk
@@ -6,8 +6,7 @@ LOCAL_SRC_FILES += \
     driver_test.c \
 
 LOCAL_MODULE:= hsi2s_test
-
-LOCAL_MODULE_TAGS := optional debug
+LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS := -Wno-unused-parameter -Wno-unused-variable
 
 include $(BUILD_EXECUTABLE)
