2b68cf3ae61ca5002b2a82856d572a7c3206f52f
Adithya Naga Sai Tirumanisetti <quic_tadithya@quicinc.com>
2022-08-04 16:20:27 +0530

    hsi2s-kernel: Add Makefile, Kbuild and other files in hsi2s to support kernel 5.15 - Added Makefile, Kbuild, Android.mk to the hsi2s-kernel folder - Removed Android.mk and Kbuild from /driver - Added hsi2s-board.mk and hsi2s-product.mk to hsi2s-kernel to add hsi2s to DLKM
    Change-Id: Ibc0f5d64912fe41820ec2a3d7d4b0c48049d99aa

---
+++ Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH := $(call my-dir)
+DLKM_DIR := $(TOP)/device/qcom/common/dlkm
+
+#KBUILD_OPTIONS
+KBUILD_OPTIONS += KERNEL_ROOT=$(shell pwd)/kernel/msm-$(TARGET_KERNEL_VERSION)/
+KBUILD_OPTIONS += MODNAME=hsi2s
+KBUILD_OPTIONS += BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
+KBUILD_OPTIONS += CONFIG_HSI2S=y
+$(info value of TARGET_USES_KERNEL_PLATFORM IS '$(TARGET_USES_KERNEL_PLATFORM)')
+
+#Clear Environment Variables
+include $(CLEAR_VARS)
+#Defining the local options
+LOCAL_SRC_FILES             :=  \
+                                $(shell find $(LOCAL_PATH)/driver/ -L -type f) \
+                                $(shell find $(LOCAL_PATH)/test/generic/ -L -type f)\
+                                $(LOCAL_PATH)/Android.mk \
+                                $(LOCAL_PATH)/hsi2s-board.mk   \
+                                $(LOCAL_PATH)/hsi2s-product.mk \
+                                $(LOCAL_PATH)/Kbuild
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+LOCAL_MODULE              := hsi2s.ko
+LOCAL_MODULE_TAGS         := optional
+
+
+include $(DLKM_DIR)/Build_external_kernelmodule.mk
+include $(LOCAL_PATH)/test/generic/Android.mk
