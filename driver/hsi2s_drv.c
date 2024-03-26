4d97c040a34605cbcc2a2456e303c9462f79dd2f
Adithya Naga Sai Tirumanisetti <quic_tadithya@quicinc.com>
2024-03-26 18:26:02 +0530

    hsi2s_kernel: driver: change in driver files
    - refactoring to map of hs_if_ctl register for seting hs3 and hs4 as hsi2s mode during interface probe
    - clearing hs_if_ctl bits during reset register call
    
    Change-Id: Ib54b3ea569194f0a3de9129f903dd971a750ac7f

--- driver/hsi2s_drv.c
+++ driver/hsi2s_drv.c
@@ -551,6 +551,8 @@ static void m_assign_macros(void)
 	hsi2s_core->macro->bit_wrdma_reset = T_WRDMA_RESET;
 	hsi2s_core->macro->bit_rate_en = T_RATE_DET_EN;
 	hsi2s_core->macro->bit_rate_reset = T_RATE_DET_RESET;
+	hsi2s_core->macro->bit_sec_hs_if_en = L_LPASS_CORE_SEC_I2S_CTL;
+	hsi2s_core->macro->bit_ter_hs_if_en = L_LPASS_CORE_TER_I2S_CTL;
 	hsi2s_core->macro->regfield_i2s_lrate_offset = T_I2S_LONG_RATE_OFFSET;
 	hsi2s_core->macro->regfield_spkr_mode_sd0 = T_I2S_SPKR_MODE_SD0;
 	hsi2s_core->macro->regfield_spkr_mode_sd1 = T_I2S_SPKR_MODE_SD1;
@@ -701,6 +703,8 @@ static void l_assign_macros(void)
 	hsi2s_core->macro->bit_wrdma_reset = T_WRDMA_RESET;
 	hsi2s_core->macro->bit_rate_en = T_RATE_DET_EN;
 	hsi2s_core->macro->bit_rate_reset = T_RATE_DET_RESET;
+	hsi2s_core->macro->bit_sec_hs_if_en = L_LPASS_CORE_SEC_I2S_CTL;
+	hsi2s_core->macro->bit_ter_hs_if_en = L_LPASS_CORE_TER_I2S_CTL;
 	hsi2s_core->macro->regfield_i2s_lrate_offset = T_I2S_LONG_RATE_OFFSET;
 	hsi2s_core->macro->regfield_spkr_mode_sd0 = T_I2S_SPKR_MODE_SD0;
 	hsi2s_core->macro->regfield_spkr_mode_sd1 = T_I2S_SPKR_MODE_SD1;
@@ -994,6 +998,7 @@ static void h_audio_mux_pin(int enable)
 /* Reset the registers */
 static void reset_registers(struct hsi2s_device *hs_dev)
 {
+
 	if (hsi2s_core->target == 8155 || hsi2s_core->target == 8195) {
 		dev_info(hs_dev->dev, "Resetting muxmode register\n");
 		reg_clear(hs_dev->lpaif_muxmode);
@@ -1030,6 +1035,16 @@ static void reset_registers(struct hsi2s_device *hs_dev)
 		setbits(hs_dev->pcm_ctl, hsi2s_core->macro->bit_pcm_reset);
 		clearbits(hs_dev->pcm_ctl, hsi2s_core->macro->bit_pcm_reset);
 	}
+	if ((hsi2s_core->target == 8295) || (hsi2s_core->target == 8255)) {
+		if (hs_dev->minor_num == HS4_I2S ){
+				dev_info(hsi2s_core->dev, "reset_registers clear bit for %d", hs_dev->minor_num);
+				clearbits(hsi2s_core->lpass_core_cc_hs_if_ctl, hsi2s_core->macro->bit_ter_hs_if_en);
+		} else if (hs_dev->minor_num == HS3_I2S) {
+				dev_info(hsi2s_core->dev, "reset_registers clear bit for %d", hs_dev->minor_num);
+				clearbits(hsi2s_core->lpass_core_cc_hs_if_ctl, hsi2s_core->macro->bit_sec_hs_if_en);
+		}
+	}
+
 }
 
 /* Reset the read DMA registers */
@@ -2759,6 +2774,15 @@ static int init_default(struct hsi2s_device *hs_dev, int intf)
 
 	reset_registers(hs_dev);
 
+	if ((hsi2s_core->target == 8295) || (hsi2s_core->target == 8255)) {
+		/* set lpaif ctl to hs_if_en for hs3 and hs4*/
+		if(3 == intf)
+			setbits(hsi2s_core->lpass_core_cc_hs_if_ctl, hsi2s_core->macro->bit_sec_hs_if_en);
+		if(4 == intf)
+			setbits(hsi2s_core->lpass_core_cc_hs_if_ctl, hsi2s_core->macro->bit_ter_hs_if_en);
+
+		dev_info(hsi2s_core->dev, "the lpass core hsis ctl val:%8x \n", readl_relaxed(hsi2s_core->lpass_core_cc_hs_if_ctl));
+	}
 	/* Allocate kernel buffers */
 	ret = hsi2s_buffer_init(hs_dev);
 	if (ret < 0) {
@@ -5284,7 +5308,6 @@ static int hsi2s_probe(struct platform_device *pdev)
 	u32 target;
 	u32 rate_array[2];
 	int ret = 0;
-	void __iomem *lpass_core_hsif_ctl;
 
 
 	if (of_device_is_compatible(pdev->dev.of_node, "qcom,hsi2s-interface"))
@@ -5379,12 +5402,6 @@ static int hsi2s_probe(struct platform_device *pdev)
 					sizeof(struct hsi2s_device *),
 					GFP_KERNEL);
 
-	/*set lpass core cc to hsi2s for hs4*/
-	if (of_device_is_compatible(pdev->dev.of_node, "qcom,sa7255-hsi2s")) {
-		lpass_core_hsif_ctl = ioremap(0x390C000, 4);
-		dev_info(hsi2s_core->dev, "the lpass core hsis ctl val:%8x \n", readl_relaxed(lpass_core_hsif_ctl));
-		setbits(lpass_core_hsif_ctl,0x10);
-	}
 	/* Enable the core clocks */
 	if (target == 6155) {
 		ret = hsi2s_enable_core_clks(pdev);
@@ -5512,6 +5529,14 @@ static int hsi2s_probe(struct platform_device *pdev)
 		if (IS_ERR(hsi2s_core->lpass_core_cc_hs_if)) {
 			dev_err(hsi2s_core->dev, "ioremap failed");
 			ret = PTR_ERR(hsi2s_core->lpass_core_cc_hs_if);
+			goto err_iounmap_lpaif;
+		}
+
+		hsi2s_core->lpass_core_cc_hs_if_ctl = ioremap(L_LPASS_CORE_CC_I2S_IF_CTL, 4);
+
+		if (IS_ERR(hsi2s_core->lpass_core_cc_hs_if_ctl)) {
+			dev_err(hsi2s_core->dev, "ioremap of lpaif ctl failed");
+			ret = PTR_ERR(hsi2s_core->lpass_core_cc_hs_if_ctl);
 			goto err_iounmap_lpaif;
 		}
 	}
@@ -5733,6 +5758,7 @@ err_iounmap_lpass_tcsr:
 		iounmap(hsi2s_core->lpass_core_cc_hs_if);
 err_iounmap_lpaif:
 	iounmap(hsi2s_core->lpaif_base_va);
+	iounmap(hsi2s_core->lpass_core_cc_hs_if_ctl);
 err_disable_core_clocks:
 	if (target == 6155) {
 		hsi2s_disable_core_clks(pdev);
