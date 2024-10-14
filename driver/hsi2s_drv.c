8a98d3cdcdab98635e207160f54dcf7c59efdd4b
Adithya Naga Sai Tirumanisetti <quic_tadithya@quicinc.com>
2024-10-14 16:18:23 +0530

    driver: change in suspend and resume callback
    - resetting registers during suspend callback
    - reset and configure normal mode in resume callback
    
    Change-Id: Icde53b43435c8fccdfaef1d2110e224ebefd07f0

--- driver/hsi2s_drv.c
+++ driver/hsi2s_drv.c
@@ -5958,6 +5958,8 @@ static int hsi2s_suspend(struct platform_device *pdev, pm_message_t state)
 
 	if (of_device_is_compatible(pdev->dev.of_node,
 				    "qcom,hsi2s-interface")) {
+		/* Call reset_registers before suspending the core clocks */
+		reset_registers((struct hsi2s_device *)platform_get_drvdata(pdev));
 		/* Set the GPIOs in sleep state */
 		ret = hsi2s_configure_gpio_pins(pdev, 0);
 		if (ret < 0) {
@@ -6040,6 +6042,7 @@ static int hsi2s_resume(struct platform_device *pdev)
 
 	if (of_device_is_compatible(pdev->dev.of_node,
 				    "qcom,hsi2s-interface")) {
+		hs_dev  = (struct hsi2s_device *)platform_get_drvdata(pdev);
 		/* Resume the interface clocks */
 		if (hsi2s_core->target == 6155) {
 			ret = hsi2s_resume_intf_clks(pdev);
@@ -6053,6 +6056,20 @@ static int hsi2s_resume(struct platform_device *pdev)
 		if (ret < 0) {
 			dev_err(&pdev->dev, "Failed to set gpios in active state");
 			goto err_resume;
+		}
+		/* Reset Registers from S2R/S2D */
+		ioctl_handler3(hs_dev, LPAIF_RESET, 0);
+		/* Configure the operational mode */
+		if (operation_mode) {
+			/* Setting slave mode for SA8155/SA8195 targets */
+			if (hsi2s_core->target == 8155 || hsi2s_core->target == 8195)
+				configure_muxmode(hs_dev, 1);
+			configure_normal_mode(hs_dev, hs_dev->minor_num );
+		}
+		else {
+			if (hsi2s_core->target == 8155 || hsi2s_core->target == 8195)
+				configure_muxmode(hs_dev, 0);
+			configure_int_loopback_mode(hs_dev, hs_dev->minor_num);
 		}
 #if ((defined(CONFIG_QTI_GVM) || defined(CONFIG_QTI_QUIN_GVM)) && defined(CONFIG_MSM_HAB))
                 ret = resume_via_hab();
@@ -6110,57 +6127,6 @@ static int hsi2s_resume(struct platform_device *pdev)
 				}
 			}
 
-			if (hsi2s_core->suspend_to_disk_trigger_flg) {
-			for(i=0;i<3;i++){
-				hs_dev = hsi2s_core->hsi2s_arr[i];
-				/*LPAIF Reset sequence*/
-				dev_info(hs_dev->dev, "Resetting muxmode register\n");
-				reg_clear(hs_dev->lpaif_muxmode);
-				if (hs_dev->lpaif_mode == HS_I2S) {
-					dev_info(hs_dev->dev, "Resetting I2S control register\n");
-					reg_clear(hs_dev->i2s_ctl);
-					dev_info(hs_dev->dev, "Resetting DMA registers\n");
-					reset_rddma_registers(hs_dev);
-					reset_wrdma_registers(hs_dev);
-					/* Clear IRQs */
-					clear_irqs();
-					/* Reset buffer pointers */
-					hs_dev->read_buffer->last_copy = 1;
-					hs_dev->read_buffer->last_xfer = 1;
-					hs_dev->write_buffer->head = hs_dev->lpass_wrdma_start;
-					hs_dev->write_buffer->tail = hs_dev->lpass_wrdma_start;
-					hs_dev->write_buffer->data_ready = 0;
-					hs_dev->write_buffer->pollin = 0;
-					hs_dev->rddma_xfer_busy = 0;
-					hs_dev->rddma_copy_busy = 1;
-					hs_dev->rddma_in_progress = 0;
-				} else {
-					dev_info(hs_dev->dev, "Resetting PCM control registers\n");
-					reg_clear(hs_dev->pcm_ctl);
-					reg_clear(hs_dev->tdm_ctl);
-					reg_clear(hs_dev->tdm_sample_width);
-					dev_info(hs_dev->dev, "Resetting DMA registers\n");
-					reset_rddma_registers(hs_dev);
-					reset_wrdma_registers(hs_dev);
-					/* Clear IRQs */
-					clear_irqs();
-					/* Reset buffer pointers */
-					hs_dev->read_buffer->last_copy = 1;
-					hs_dev->read_buffer->last_xfer = 1;
-					hs_dev->write_buffer->head = hs_dev->lpass_wrdma_start;
-					hs_dev->write_buffer->tail = hs_dev->lpass_wrdma_start;
-					hs_dev->write_buffer->data_ready = 0;
-					hs_dev->write_buffer->pollin = 0;
-					hs_dev->rddma_xfer_busy = 0;
-					hs_dev->rddma_copy_busy = 1;
-					hs_dev->rddma_in_progress = 0;
-				}
-				/*Normal Rx sequence*/
-				configure_muxmode(hs_dev, 1);
-				configure_normal_mode(hs_dev, i);
-			}
-			hsi2s_core->suspend_to_disk_trigger_flg= false;
-			}
 		}
 	}
 
